89ac7aff9c62e800f548811b0765ee27195b80b0
James Sargant <shorinbonsai@gmail.com>
2022-03-15 00:24:33 -0400

    refactor - need add elite

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::cmp;
 use std::iter::FromIterator;
 use std::ops::Index;
 
+use rand::distributions::{Distribution, Uniform};
 use rand::prelude::*;
 use rand::seq::SliceRandom;
 use rand::Rng;
@@ -9,132 +10,9 @@ use rand::RngCore;
 use rand::SeedableRng;
 use rand_chacha::ChaCha8Rng;
 
-// fn UniformCrossover(
-//     rng: &mut dyn RngCore,
-//     parent_1: &Chromosome,
-//     parent_2: &Chromosome,
-//     compat_matrix: &Vec<Vec<u32>>,
-// ) -> Chromosome {
-//     assert_eq!(parent_1.len(), parent_2.len());
-//     if rng.gen_bool(self.chance_cross as _) {
-//         let mut cloned_matrix = compat_matrix.clone();
-//         let mut child: Vec<u32> = Vec::new();
-//         let num_genes = parent_1.len();
-//         let mut cand_codes_vec: Vec<u32> = vec![];
-//         let mut gene_idx = 0;
-//         // loop until chromosome is full
-//         while gene_idx < num_genes {
-//             let gene = if rng.gen_bool(0.5) {
-//                 parent_1[gene_idx]
-//             } else {
-//                 parent_2[gene_idx]
-//             };
-//             if child.len() == 0 {
-//                 child.push(gene);
-//                 cand_codes_vec = cloned_matrix[0].drain((gene + 1) as usize..).collect();
-//             } else {
-//                 let mut flag: bool = true;
-
-//                 for j in &child {
-//                     let new_code = cand_codes_vec[*j as usize];
-//                     let indx = compat_matrix[0]
-//                         .iter()
-//                         .position(|&x| x == new_code)
-//                         .unwrap();
-//                     let child_indx = compat_matrix[0]
-//                         .iter()
-//                         .position(|&y| y == cand_codes_vec[gene as usize])
-//                         .unwrap();
-//                     if compat_matrix[indx][child_indx] == 0 {
-//                         flag = false;
-//                         continue;
-//                     }
-//                 }
-//                 if flag == true {
-//                     child.push(gene);
-//                     cand_codes_vec = cand_codes_vec.drain((gene + 1) as usize..).collect();
-//                     gene_idx += 1;
-//                 }
-//             }
-//         }
-//         child.into_iter().collect()
-//     } else {
-//         let child: Vec<u32> = parent_1.genes.clone();
-//         child.into_iter().collect()
-//     }
-// }
-
-// pub trait MutationMethod {
-//     fn mutate(&self, rng: &mut dyn RngCore, child: &mut Chromosome, compat_matrix: &Vec<Vec<u32>>);
-// }
-
-// #[derive(Clone, Debug)]
-// pub struct CodeMutation {
-//     chance_mut: f32,
-// }
-
-// impl CodeMutation {
-//     pub fn new(chance_mut: f32) -> Self {
-//         assert!(chance_mut >= 0.0 && chance_mut <= 1.0);
-//         Self { chance_mut }
-//     }
-// }
-
-// impl MutationMethod for CodeMutation {
-//     fn mutate(&self, rng: &mut dyn RngCore, child: &mut Chromosome, compat_matrix: &Vec<Vec<u32>>) {
-//         if rng.gen_bool(self.chance_mut as _) {
-//             let element = rng.gen_range(0..child.genes.len());
-//             child.genes.remove(element);
-//             let mut cloned_matrix = compat_matrix[0].clone();
-//             cloned_matrix.remove(0);
-//             let mut child_codes = words_from_chrom(&child.genes, &cloned_matrix);
-//             let tmp_child_codes = child_codes.clone();
-//             let mut done: bool = false;
-//             for i in &cloned_matrix {
-//                 if done {
-//                     break;
-//                 }
-//                 for j in &tmp_child_codes {
-//                     if done {
-//                         break;
-//                     }
-//                     if *i == *j {
-//                         break;
-//                     } else if !check_compat(*i, *j, compat_matrix) {
-//                         break;
-//                     } else {
-//                         match child_codes.binary_search(&i) {
-//                             Ok(_pos) => {}
-//                             Err(pos) => child_codes.insert(pos, *i),
-//                         }
-//                         child.genes = chrom_from_words(&child_codes, &cloned_matrix);
-//                         done = true;
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// }
-
-// pub trait SelectionMethod {
-//     fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
-//     where
-//         I: Individual;
-// }
-
-// #[derive(Clone, Debug, Default)]
-// pub struct RouletteWheelSelection;
-
-// impl SelectionMethod for RouletteWheelSelection {
-// fn select<'a, I>(&self, rng: &mut dyn RngCore, population: &'a [I]) -> &'a I
-// where
-//     I: Individual,
-// {
-//     population
-//         .choose_weighted(rng, |individual| individual.fitness())
-//         .expect("got an empty population")
-// }
-// }
+const NUM_GEN: i32 = 100;
+const MUT_CHANCE: f32 = 0.15;
+const CROSS_CHANCE: f32 = 0.85;
 
 //utility functions
 fn minimum(a: u32, b: u32, c: u32) -> u32 {
@@ -277,51 +155,94 @@ pub fn create_candidates(n: u32) -> Vec<u32> {
     let codes: Vec<u32> = (0..(2_u32.pow(n))).collect();
     codes
 }
-
+#[derive(Clone)]
 pub struct Individual {
     chromosome: Vec<u32>,
     fitness: u32,
+    chance_mut: f32,
 }
 
 impl Individual {
-    fn new(chromosome: Vec<u32>, fitness: u32) -> Individual {
+    fn new(chromosome: Vec<u32>, fitness: u32, chance_mut: f32) -> Individual {
         Individual {
             chromosome: chromosome,
             fitness: fitness,
+            chance_mut: chance_mut,
         }
     }
-    //greedy lexicode algorithm for fitness
-    fn fitness(lexicodes: &mut Vec<u32>, compat: &Vec<Vec<u32>>) -> (Vec<u32>, u32) {
-        let clength = lexicodes.len();
-        let mut candidates: Vec<u32> = compat[0].clone();
-        let maxvalue = candidates.iter().max().unwrap();
-        let indx1 = candidates.iter().position(|&x| x == *maxvalue).unwrap();
-        candidates = candidates.drain((indx1 + 1)..).collect();
-        for i in 0..candidates.len() {
-            if clength >= 1 {
-                let mut flag: bool = true;
-                for j in lexicodes.iter() {
-                    if !check_compat(candidates[i], *j, compat) {
-                        flag = false;
-                        continue;
-                    }
-                }
-                if flag == true {
-                    lexicodes.push(candidates[i]);
-                }
-            }
-        }
-        let fit: u32 = lexicodes.len() as u32;
-        return (lexicodes.to_vec(), fit);
+
+    pub fn len(&self) -> usize {
+        self.chromosome.len()
+    }
+
+    pub fn iter(&self) -> impl Iterator<Item = &u32> {
+        self.chromosome.iter()
+    }
+
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut u32> {
+        self.chromosome.iter_mut()
     }
 }
 
+pub fn mutate(
+    rng: &mut dyn RngCore,
+    child: &mut Individual,
+    compat_matrix: &Vec<Vec<u32>>,
+    chance_mut: f32,
+) {
+    if rng.gen_bool(chance_mut as _) {
+        let element = rng.gen_range(0..child.chromosome.len());
+        child.chromosome.remove(element);
+        let mut cloned_matrix = compat_matrix[0].clone();
+        cloned_matrix.remove(0);
+        let mut child_codes = words_from_chrom(&child.chromosome, &cloned_matrix);
+        let tmp_child_codes = child_codes.clone();
+        let mut done: bool = false;
+        for i in &cloned_matrix {
+            if done {
+                break;
+            }
+            for j in &tmp_child_codes {
+                if done {
+                    break;
+                }
+                if *i == *j {
+                    break;
+                } else if !check_compat(*i, *j, compat_matrix) {
+                    break;
+                } else {
+                    match child_codes.binary_search(&i) {
+                        Ok(_pos) => {}
+                        Err(pos) => child_codes.insert(pos, *i),
+                    }
+                    child.chromosome = chrom_from_words(&child_codes, &cloned_matrix);
+                    done = true;
+                }
+            }
+        }
+    }
+}
+
+impl Index<usize> for Individual {
+    type Output = u32;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.chromosome[index]
+    }
+}
+#[derive(Clone)]
 pub struct Population {
     pop: Vec<Individual>,
 }
 
 impl Population {
-    fn gen_pop(popsize: u32, n: u32, d: u32, chrom_size: u32) -> (Vec<Individual>, Vec<Vec<u32>>) {
+    fn gen_pop(
+        popsize: u32,
+        n: u32,
+        d: u32,
+        chrom_size: u32,
+        chance_mut: f32,
+    ) -> (Vec<Individual>, Vec<Vec<u32>>) {
         let mut rng = rand::thread_rng();
         let cand: Vec<u32> = create_candidates(n);
         let compat = compat_matrix(&cand, d);
@@ -345,7 +266,7 @@ impl Population {
                 tmp.push(candidates[indx]);
             }
             //init individual to add to population
-            let tmp_ind = Individual::new(tmp, 0_u32);
+            let tmp_ind = Individual::new(tmp, 0_u32, chance_mut);
             pop.push(tmp_ind);
         }
         (pop, compat)
@@ -363,30 +284,139 @@ impl Population {
             let indiv_share = indiv.fitness as f32 / total_fitness;
 
             if rng.gen_bool(indiv_share as f64) {
-                return *indiv;
+                return indiv.clone();
             }
         }
     }
+
+    pub fn UniformCrossover(
+        rng: &mut dyn RngCore,
+        parent_1: &Individual,
+        parent_2: &Individual,
+        compat_matrix: &Vec<Vec<u32>>,
+        cross_chance: f32,
+    ) -> Individual {
+        if rng.gen_bool(cross_chance as _) {
+            let crossoverpoint = Uniform::new(1, parent_1.chromosome.len()).sample(rng);
+            let mut child: Vec<u32> = parent_1.chromosome.clone();
+            let dispose: Vec<u32> = child.drain(crossoverpoint..).collect();
+            let mut p2_dna = parent_2.chromosome.clone();
+            p2_dna = p2_dna.drain(crossoverpoint..).collect();
+            child.extend_from_slice(&p2_dna);
+            let new = Individual::new(child, 0_u32, MUT_CHANCE);
+            new
+        } else {
+            let new = Individual::new(parent_1.chromosome.clone(), 0, MUT_CHANCE);
+            new
+        }
+        // assert_eq!(parent_1.chromosome.len(), parent_2.chromosome.len());
+        //     if parent_1.chromosome.len() != parent_2.chromosome.len() {
+        //         println!("Diff length chromosome...");
+        //     }
+        //     if rng.gen_bool(cross_chance as _) {
+        //         let mut cloned_matrix = compat_matrix.clone();
+        //         let mut child: Vec<u32> = Vec::new();
+        //         let num_genes = parent_1.chromosome.len();
+        //         let mut cand_codes_vec: Vec<u32> = vec![];
+        //         let mut gene_idx = 1;
+        //         // loop until chromosome is full
+        //         while gene_idx < num_genes {
+        //             let gene = if rng.gen_bool(0.5) {
+        //                 parent_1[gene_idx]
+        //             } else {
+        //                 parent_2[gene_idx]
+        //             };
+        //             if child.len() == 0 {
+        //                 child.push(gene);
+        //                 cand_codes_vec = cloned_matrix[0].drain((gene + 1) as usize..).collect();
+        //             } else {
+        //                 let mut flag: bool = true;
+
+        //                 for j in &child {
+        //                     let new_code = cand_codes_vec[*j as usize];
+        //                     let indx = compat_matrix[0]
+        //                         .iter()
+        //                         .position(|&x| x == new_code)
+        //                         .unwrap();
+        //                     let child_indx = compat_matrix[0]
+        //                         .iter()
+        //                         .position(|&y| y == cand_codes_vec[gene as usize])
+        //                         .unwrap();
+        //                     if compat_matrix[indx][child_indx] == 0 {
+        //                         flag = false;
+        //                         continue;
+        //                     }
+        //                 }
+        //                 if flag == true {
+        //                     child.push(gene);
+        //                     cand_codes_vec = cand_codes_vec.drain((gene + 1) as usize..).collect();
+        //                     gene_idx += 1;
+        //                 }
+        //             }
+        //         }
+        //         let mut child_individual = Individual::new(child, 0, cross_chance);
+        //         child_individual
+        //     } else {
+        //         let child: Vec<u32> = parent_1.chromosome.clone();
+        //         let mut child_individual = Individual::new(child, 0, cross_chance);
+        //         child_individual
+        //     }
+    }
+}
+
+//greedy lexicode algorithm for fitness
+fn fitness(lexicodes: &mut Vec<u32>, compat: &Vec<Vec<u32>>) -> (Vec<u32>, u32) {
+    let clength = lexicodes.len();
+    let mut candidates: Vec<u32> = compat[0].clone();
+    let maxvalue = lexicodes.iter().max().unwrap();
+    let indx1 = candidates.iter().position(|&x| x == *maxvalue).unwrap();
+    candidates = candidates.drain((indx1 + 1)..).collect();
+    for i in 0..candidates.len() {
+        if clength >= 1 {
+            let mut flag: bool = true;
+            for j in lexicodes.iter() {
+                if !check_compat(candidates[i], *j, compat) {
+                    flag = false;
+                    continue;
+                }
+            }
+            if flag == true {
+                lexicodes.push(candidates[i]);
+            }
+        }
+    }
+    let fit: u32 = lexicodes.len() as u32;
+    return (lexicodes.to_vec(), fit);
 }
 
 pub fn evolve(
     rng: &mut dyn RngCore,
-    population: &[I],
+    population: &Vec<Individual>,
     compat_matrix: &Vec<Vec<u32>>,
-    // chance_cross: f64,
+    chance_cross: f32,
+    chance_mut: f32,
 ) -> Vec<Individual> {
     assert!(!population.is_empty());
-    (0..population.len())
-        .map(|_| {
-            let parent_1 = Population::roulette_select(rng, population);
-            let parent_2 = Population::roulette_select(rng, population);
-            let mut child = self
-                .crossover_meth
-                .crossover(rng, parent_1, parent_2, compat_matrix);
+    let mut new_pop: Vec<Individual> = vec![];
+    for _ in 0..population.len() {
+        let parent_1 = Population::roulette_select(rng, population);
+        let parent_2 = Population::roulette_select(rng, population);
+        let mut child =
+            Population::UniformCrossover(rng, &parent_1, &parent_2, compat_matrix, chance_cross);
+        mutate(rng, &mut child, compat_matrix, chance_mut);
+        new_pop.push(child)
+    }
+    new_pop
+}
 
-            self.mutation_meth.mutate(rng, &mut child, compat_matrix);
-        })
-        .collect()
+pub fn get_fitness(populat: &mut Population, compat_matrix: &Vec<Vec<u32>>) {
+    let mut candidates = compat_matrix[0].clone();
+    let discard = candidates.remove(0);
+    for mut i in &mut populat.pop {
+        let mut words = words_from_chrom(&i.chromosome, &candidates);
+        let (codes, fit) = fitness(&mut i.chromosome, &compat_matrix);
+        i.fitness = fit;
+    }
 }
 
 fn main() {
@@ -395,10 +425,33 @@ fn main() {
     let mut rng = ChaCha8Rng::from_seed(Default::default());
     let init: Vec<Individual> = vec![];
     let mut test = Population { pop: init };
-    let (pop1, compat_matrix) = Population::gen_pop(5, 6, 3, 7);
-    let population = Population { pop: pop1 };
 
+    //GA init
+    let (pop1, compat_matrix) = Population::gen_pop(50, 6, 3, 7, MUT_CHANCE);
+    let mut candidates = compat_matrix[0].clone();
+    candidates = candidates.drain(1..).collect();
+    let mut population = Population { pop: pop1 };
+    get_fitness(&mut population, &compat_matrix);
     //GA loop
+    for i in 0..NUM_GEN {
+        evolve(
+            &mut rng,
+            &population.pop,
+            &compat_matrix,
+            CROSS_CHANCE,
+            MUT_CHANCE,
+        );
+        get_fitness(&mut population, &compat_matrix);
+        println!("Gen: {}", i);
+    }
+    for i in population.pop {
+        println!("number words: {}  ", i.fitness);
+        let mut seed_words = words_from_chrom(&i.chromosome, &candidates);
+        let (mut words, fit) = fitness(&mut seed_words, &compat_matrix);
+        for j in &words {
+            println!(" words: {:06b}", *j);
+        }
+    }
 
     // let genetic_algo = GeneticAlgorithm::new(
     //     RouletteWheelSelection::default(),
@@ -431,15 +484,4 @@ fn main() {
     // let x_vec: Vec<char> = x.chars().collect();
     // let y = format!("{:08b}", codes[2]);
     // let y_vec: Vec<char> = y.chars().collect();
-
-    // let testy = levenshtein_d1stance(&x, &y);
-    // let s = format!("{:08b}", x);
-    // println!("x is: {:?} or {}", x_vec, codes[5]);
-    // println!("y is: {:?} or {}", y_vec, codes[2]);
-    // println!("The levenshtein dist is: {}", testy);
-    // println!("Hamming distance is: {}", hamming_dist(codes[5], codes[2]));
-    // println!("Edit distance: {}", edit_distance(&x, &y));
-    // let inty = s.parse::<u32>().unwrap();
-
-    // println!("{}", inty);
 }
